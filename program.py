d474316bab8bdfbe7edb85f8e12f7d38e504de95
malakahmed12w-gif <malakahmed12w@gmail.com>
2025-08-30 18:29:56 +0200

    commit

---
+++ program.py
@@ -0,0 +1,8 @@
+def rectangle_area(width, height):
+    return width * height
+
+# Example usage
+if __name__ == "__main__":
+    w = float(input("Enter width: "))
+    h = float(input("Enter height: "))
+    print("Area of rectangle:", rectangle_area(w, h))
